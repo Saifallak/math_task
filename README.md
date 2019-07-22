f223a30e41eba795e2f24e31da6180fc9ddc39eb
Saif Allah Khaled <Saifallak@yahoo.com>
2019-07-22 19:34:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A sample command-line application.
+
+Command line application just multiples 1 * 1 to 10 * 10 and prints them
---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+main(List<String> arguments) {
+  for (int x = 1; x <= 10; x++) {
+    for (int y = 1; y <= 10; y++) {
+      int z = x * y;
+      print("$x * $y = $z");
+    }
+  }
+}
